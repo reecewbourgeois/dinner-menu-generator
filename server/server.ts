47cb1775c90300fa25d250d8d6e6b46a541689c5
Reece Bourgeois <reecebourgeois@gmail.com>
2021-10-19 00:22:52 -0500

    changed & added some comments, removed Mongo imports

--- server/server.ts
+++ server/server.ts
@@ -1,41 +1,27 @@
 import express from "express";
 import path from "path";
-import { MongoClient } from "mongodb";
-import { mongodb_uri } from "./config";
 
+/* Create an express application */
 const app = express();
 
-//Set up client connection info
-const client = new MongoClient(mongodb_uri);
-
+/* Set port */
 app.set("port", process.env.PORT || 3000);
+
+/* Allow the parsing of JSON */
 app.use(express.json());
 
-//Test route
-//app.use("/api/test", require("./routes/test"));
-
-//Backend Routes
+/* Backend Routes */
 app.use("/api/addMealToDatabase", require("./routes/addMealToDatabase"));
 
-//All are needed to prevent crashing on page refresh
-//app.set("views", path.join(__dirname, "views"));
+/* All are needed to prevent crashing on page refresh*/
 app.use(express.static("../client/build"));
 app.get("*", (req: any, res: { sendFile: (arg0: any) => void }) => {
   res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
 });
 
-//Connect to database and start the app
-client.connect((err: any, database: any) => {
-  if (err) {
-    console.log(err);
-  }
-
-  //Allows for database operation in other files without starting a whole new connection every time
-  app.set("database", database);
-
-  app.listen(app.get("port"), () => {
-    console.log("Express server listening on port " + app.get("port"));
-  });
+/* Start the App */
+app.listen(app.get("port"), () => {
+  console.log("Express server listening on port " + app.get("port"));
 });
 
 module.exports = app;
